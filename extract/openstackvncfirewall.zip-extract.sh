a94acc88482ed6312e99a55f9a8d78d662b0e24e
R. C. Pao <rpao-depot-git@paonet.org>
2019-09-15 13:59:32 -0700

    Bug fixes. Added example run outputs from Linux and then from ESXi 6.7u2.

--- extract/openstackvncfirewall.zip-extract.sh
+++ extract/openstackvncfirewall.zip-extract.sh
@@ -1,30 +1,52 @@
 #!/bin/sh -x
 
-mkdir extracted
+
+[ ! -d extracted ] && mkdir extracted
 cd extracted
-unzip -n ../../openstackvncfirewall.zip
-cd vib20/OpenStackVNC
+unzip -o ../../openstackvncfirewall.zip
+ls -l vendor-index.xml metadata.zip index.xml vib20/OpenStackVNC/*.vib
+chmod 0644 vendor-index.xml metadata.zip index.xml vib20/OpenStackVNC/*.vib
+ls -l vendor-index.xml metadata.zip index.xml vib20/OpenStackVNC/*.vib
 
+
+# VMware_bootbank_OpenStackVNC_5.0.0-0.0.1.vib -> vnc
 # http://www.yellow-bricks.com/2011/11/29/how-to-create-your-own-vib-files/
-ar vx VMware_bootbank_OpenStackVNC_5.0.0-0.0.1.vib
-chmod 700 vnc
-
-# https://www.virtuallyghetto.com/2011/08/how-to-create-and-modify-vgz-vmtar.html
-#gunzip < vnc > vnc.xz
-#ls vnc.xz
-#xz --single-stream --decompress < vnc.xz > vnc.vtar
-
-# -ao[a=overwite_all_existing|s=skip_existing|u=rename_extracting_file|t=rename_existing_file]
-7z x -aos vnc
-ls vnc.tar.vtar
-
-if [ ! -x /bin/vmtar ]; then
-  echo /bin/vmtar only exists in VMware ESXi.
-  exit
+cd vib20/OpenStackVNC
+if [ ! -f vnc ]; then
+  if [ ! -x /usr/bin/ar ]; then
+    echo /usr/bin/ar does not exist in ESXi. Try Linux.
+    exit
+  fi
+  /usr/bin/ar vx VMware_bootbank_OpenStackVNC_5.0.0-0.0.1.vib
 fi
-vmtar -v -x vnc.tar.vtar -o vnc.tar
-[ -x /bin/vmtar ] && vmtar -v -x vnc.tar.vtar -o vnc.tar
-ls vnc.tar
+ls -l descriptor.xml sig.pkcs7 vnc
+chmod 0644 descriptor.xml sig.pkcs7 vnc
+ls -l descriptor.xml sig.pkcs7 vnc
 
-# tar -tvf vnc.tar
-tar -xvf vnc.tar
+
+# vnc -> vnc.tar.vtar
+gunzip < vnc > vnc.tar.vtar
+ls -l vnc.tar.vtar
+
+
+# vnc.tar.vtar -> vnc.tar
+if [ ! -f vnc.tar ]; then
+  if [ -f vnc.tar.vtar ]; then
+
+    if [ ! -x /bin/vmtar ]; then
+      echo /bin/vmtar only exists in VMware ESXi.
+      echo Copy vnc.tar.vtar to ESXi and rerun.
+      exit
+    fi
+    vmtar -v -x vnc.tar.vtar -o vnc.tar
+  fi
+fi
+
+
+ls -l vnc.tar
+tar xvf vnc.tar
+ls -l etc/vmware/firewall/vnc.xml
+cat etc/vmware/firewall/vnc.xml
+echo "done"
+exit
+
